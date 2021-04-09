a2eb38dcaea838ee37f2416c748e2ab6d0dac6c9
tristan <westenskowtristan122@gmail.com>
2021-04-08 21:30:57 -0600

    added database tables and values

---
+++ db/connection.js
@@ -0,0 +1,16 @@
+// Connect to database
+const mysql = require("mysql2");
+
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    //Your MySQL username,
+    user: "root",
+    //Your MySQL password
+    password: "Frublsnap12",
+    database: "teamtracker",
+  },
+  console.log("Connected to the teamtracker database.")
+);
+
+module.exports = db;
---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+INSERT INTO department (name)
+VALUES
+('sales'),
+('HR'),
+('Engineer'),
+('Developer');
+
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+('Marketing Director', 80.000, 1),
+('Web Designer', 100.000, 2),
+('Project Manager', 120.000, 4),
+('President of Sales', 80.000, 2),
+('Accountant Executive', 200.000, 2);
+
+INSERT INTO employee (first_name, last_name, role_id, department_id)
+VALUES
+ ('James', 'Fraser', 2, 2),
+  ('Jack', 'London', 1, 1),
+  ('Robert', 'Bruce', 4, 2),
+  ('Peter', 'Greenaway', 3, 2),
+  ('Derek', 'Jarman', 4, 2),
+  ('Paolo', 'Pasolini', 2, 3),
+  ('Heathcote', 'Williams', 1, 2);
